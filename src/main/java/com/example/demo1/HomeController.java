ec8bf76c364140c404f78b3efed84d8ee54169b8
HiepH <youremail@example.com>
2025-05-10 11:50:36 +0700

    uodate

---
+++ src/main/java/com/example/demo1/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Xin chào từ Spring Boot!");
+        return "home";
+    }
+}
